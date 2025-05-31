a998753b1eb303eb9b834ddbdc83ac6cae1c82a1
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 09:55:34 -0700

    Comprehensive types

--- src/cycles.ts
+++ src/cycles.ts
@@ -1,8 +1,8 @@
 import type Graph from './Graph.js';
 
-import type { Key } from './types.js';
+import type { Cycle, Key } from './types.js';
 
-export default function cycles<T extends Key>(graph: Graph<T>): T[][] {
+export default function cycles<T extends Key>(graph: Graph<T>): Cycle<T>[] {
   const visited = {};
   const marks = {};
   const cycles = [];
