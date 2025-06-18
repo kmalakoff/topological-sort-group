ab3f857890c46d280acbe4f48cd2a75680b67e6d
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:41:23 -0700

    Update Biome configuration and CI node version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './types.js';
-export { default as Graph } from './Graph.js';
 export { default as cycles } from './cycles.js';
+export { default as Graph } from './Graph.js';
 export { default as sort } from './sort.js';
+export * from './types.js';
