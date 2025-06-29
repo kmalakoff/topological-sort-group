08e73c353b7b12cbf94b0527b2801bfcdc4b96cb
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-29 14:33:25 -0700

    Upgrade modules

--- test/exports/import.test.ts
+++ test/exports/import.test.ts
@@ -2,20 +2,18 @@ import assert from 'assert';
 // @ts-ignore
 import * as topo from 'topological-sort-group';
 // @ts-ignore
-import { cycles, Graph, SortMode, sort } from 'topological-sort-group';
+import Graph, { SortMode } from 'topological-sort-group';
 
 describe('exports .ts', () => {
   it('exports', () => {
-    assert.equal(typeof topo.Graph, 'function');
-    assert.equal(typeof topo.cycles, 'function');
-    assert.equal(typeof topo.sort, 'function');
+    assert.equal(typeof topo.default, 'function');
+    assert.equal(typeof topo.default.SortMode.Group, 'number');
     assert.equal(typeof topo.SortMode.Flat, 'number');
   });
 
   it('named exports', () => {
     assert.equal(typeof Graph, 'function');
-    assert.equal(typeof cycles, 'function');
-    assert.equal(typeof sort, 'function');
+    assert.equal(typeof Graph.SortMode.Group, 'number');
     assert.equal(typeof SortMode.Flat, 'number');
   });
 });
